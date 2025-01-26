5ae4ba0f1e3beeecd828ac8d28fe0f0b6c494326
MatiasSF04 <matias.sf04@gmail.com>
2025-01-26 19:16:43 -0300

    Add listing tester

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-function agregarAmigos() {
+function agregarAmigo() {
     let nombre = document.getElementById('amigo').value;
     if (nombre != ""){
         amigos.push(nombre);
@@ -10,3 +10,5 @@ function agregarAmigos() {
     }
     nombre = '';
 }
+
+console.log(amigos);
