43867b2260500950f6172505d0405408690c34df
MatiasSF04 <matias.sf04@gmail.com>
2025-01-26 18:50:40 -0300

    Actualizar test

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigos() {
+    let nombre = document.getElementById('amigo').value;
+    if (nombre != ""){
+        amigos.push(nombre);
+    } else {
+        alert('Por favor, inserte un nombre.');
+    }
+    nombre = '';
+}
