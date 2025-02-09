b5df84a16b3ce75a865fa95f1b5330ea9726cf4a
MatiasSF04 <matias.sf04@gmail.com>
2025-02-09 15:01:06 -0300

    Create menu list

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 let nombreLista = document.getElementById('listaAmigos');
+
+function mostrarMenu() {
+    menu = document.getElementsByClassName('menu');
+    CSSStyleValue()
+}
 
 function agregarAmigo() {
     let nombre = document.getElementById('amigo').value;
